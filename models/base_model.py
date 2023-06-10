bf3fc5a3b221be0ce7574013c3363f7a1e225762
PaintRock <6126@holbertonstudents.com>
2023-06-10 11:15:35 -0700

    save issue

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ the other classes"""
 import uuid
 from datetime import datetime
 import models
-
+make_time = '%Y-%m-%dT%H:%M:%S.%f'
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -12,16 +12,16 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-            self.created_at = datetime.strptime(
-                self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(
-                self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.strptime(self.created_at, make_time)
+            self.updated_at = datetime.strptime(self.updated_at, make_time)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
