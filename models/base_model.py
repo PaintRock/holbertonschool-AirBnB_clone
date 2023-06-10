3019b0d9992fb5d79c029002d1e47a1595ad00e9
PaintRock <6126@holbertonstudents.com>
2023-06-10 12:05:06 -0700

    undo

--- models/base_model.py
+++ models/base_model.py
@@ -18,11 +18,9 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
