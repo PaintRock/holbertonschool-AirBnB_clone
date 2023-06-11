d6d02f3eae48aac0da03ee36cb3d476aa239da2e
PaintRock <6126@holbertonstudents.com>
2023-06-11 17:47:26 -0500

    w

--- models/base_model.py
+++ models/base_model.py
@@ -8,32 +8,50 @@ time_maker = '%Y-%m-%dT%H:%M:%S.%f'
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        
+def __init__(self, *args, **kwargs):
+        """
+        Instantiate public attributes
+        """
         if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-            # Handle 'created_at' and 'updated_at' if present in kwargs
-            if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs['created_at'], time_maker)
-            if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs['updated_at'], time_maker)
-        else:
-            self.created_at = self.updated_at = datetime.now()
+            for key in kwargs.keys():
+                if key == "created_at":
+                    setattr(self, key,
+                            datetime.strptime(kwargs[key], time_format))
+                elif key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(kwargs[key], time_format))
+                elif key != ('__class__'):
+                    setattr(self, key, kwargs[key])
 
-    def save(self):
-        self.updated_at = datetime.now()
-        # models.storage.save() implementation is missing
-        
-    def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        """
+        Override the string to print [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute updated_at
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
+        class_dict = self.__dict__.copy()
+        class_dict["__class__"] = self.__class__.__name__
+        class_dict["created_at"] = self.created_at.isoformat()
+        class_dict["updated_at"] = self.updated_at.isoformat()
+        return class_dict
