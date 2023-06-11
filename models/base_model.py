9971cc5d2d503ebe7840fb4341ad964b7e8a155d
PaintRock <6126@holbertonstudents.com>
2023-06-11 17:35:56 -0500

    kwarg

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,22 @@ time_maker = '%Y-%m-%dT%H:%M:%S.%f'
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-    if kwargs:
-        for key, value in kwargs.items():
-            if key != '__class__':
-                setattr(self, key, value)
-        self.created_at = datetime.strptime(self.created_at, time_maker)
-        self.updated_at = datetime.strptime(self.updated_at, time_maker)
-    else:
-        self.created_at = self.updated_at = datetime.now()
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            # Handle 'created_at' and 'updated_at' if present in kwargs
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], time_maker)
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], time_maker)
+        else:
+            self.created_at = self.updated_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.save()
+        # models.storage.save() implementation is missing
         
     def to_dict(self):
         obj_dict = self.__dict__.copy()
