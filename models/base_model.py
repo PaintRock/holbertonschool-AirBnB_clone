ee76893eea7de9cc6dbc7e5eae1c3bd47f788233
PaintRock <6126@holbertonstudents.com>
2023-06-11 17:24:15 -0500

    again

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ time_maker = '%Y-%m-%dT%H:%M:%S.%f'
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-    self.id = str(uuid.uuid4())
+        self.id = str(uuid.uuid4())
     if kwargs:
         for key, value in kwargs.items():
             if key != '__class__':
