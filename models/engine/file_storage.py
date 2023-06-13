1565a4912d5e891140eedb9e11f799f50bb3c97d
AAndrews-1982 <altonandrews@icloud.com>
2023-06-13 14:49:41 -0700

    fixed blank line and indentation error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,15 +17,16 @@ class FileStorage():
     __file_path = "file.json"
     __objects = {}
 
+
 def destroy_all(cls):
         """Destroys all existing instances of BaseModel and it's children"""
         cls.__objects = {}
-
-    def all(self):
+    
+def all(self):
         """returns the dictionary __objects"""
         return self.__objects
 
-    def new(self, obj):
+def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
@@ -37,14 +38,14 @@ def save(self):
                           obj in self.__objects.items()}
             json.dump(serial_obj, file)
 
-    def save(self):
+def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         with open(self.__file_path, mode='w', encoding='utf-8') as file:
             serial_obj = {key: obj.to_dict() for key,
                           obj in self.__objects.items()}
             json.dump(serial_obj, file)
 
-    def reload(self):
+def reload(self):
         """deserializes the JSON file to __objects (only if the JSON
             file (__file_path) exists ; otherwise, do nothing. If the
             file doesn’t exist, no exception should be raised)"""
