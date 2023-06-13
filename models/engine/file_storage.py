5cdf4a7d7a6b819c9576cb3daa3f29b998e9f8ec
AAndrews-1982 <altonandrews@icloud.com>
2023-06-13 13:29:18 -0700

    fixed indent error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,17 +4,16 @@ Class that serializes instances to a JSON file
     and deserializes JSON file to instances
 """
 import json
-import os
 from models.base_model import BaseModel
-# from models.user import Amenity
-# from models.user import City
-# from models.user import Place
-# from models.user import Review
-# from models.user import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
 
 
-class FileStorage:
+class FileStorage():
     """serializes instance to JSON file"""
     __file_path = "file.json"
     __objects = {}
@@ -31,8 +30,8 @@ class FileStorage:
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         with open(self.__file_path, mode='w', encoding='utf-8') as file:
-            serial_obj = {key: obj.to_dict()for key,
-                            obj in self.__objects.items()}
+            serial_obj = {key: obj.to_dict() for key,
+                          obj in self.__objects.items()}
             json.dump(serial_obj, file)
 
     def reload(self):
@@ -47,3 +46,4 @@ class FileStorage:
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
+
