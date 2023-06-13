07ddd5bd92e133ef657681ff12b7bdef2dad8429
PaintRock <6126@holbertonstudents.com>
2023-06-13 17:43:37 -0500

    file_storage blank space

--- models/README.md
+++
@@ -1,2 +0,0 @@
-This is the readme file for the Models folder in the Air B-n-B project. 
-We have an Author's page
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,22 +17,18 @@ class FileStorage():
     __file_path = "file.json"
     __objects = {}
 
-
     def destroy_all(cls):
-        """Destroys all existing instances of BaseModel and its children"""
+        """Destroys """
         cls.__objects = {}
     
-
     def all(self):
         """returns the dictionary __objects"""
         return self.__objects
-
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
-
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
@@ -40,7 +36,6 @@ class FileStorage():
             serial_obj = {key: obj.to_dict() for key,
                           obj in self.__objects.items()}
             json.dump(serial_obj, file)
-
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON
