45ac942b9a2bf2da8aa99989e4f75a847a058d88
PaintRock <6126@holbertonstudents.com>
2023-06-14 18:01:54 -0500

    ppp

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -33,6 +33,20 @@ class TestReview(unittest.TestCase):
         self.assertEqual(self.review.user_id, "")
         self.assertEqual(self.review.place_id, "")
         self.assertEqual(self.review.text, "")
-
+    
+    def test__str__(self):
+    """Test the string representation of BaseModel instance"""
+    review = Review()
+    review.id = "123"
+    review.user_id = "456"
+    review.place_id = "789"
+    review.text = "Test review"
+    
+    expected_str = "[Review] (123) {
+        'id': '123', 'user_id': '456', 'place_id': '789', 'text': 'Test review'
+        }"
+    self.assertEqual(str(review), expected_str)
+        
+   
 if __name__ == '__main__':
     unittest.main()
