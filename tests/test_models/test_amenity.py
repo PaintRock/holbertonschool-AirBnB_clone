56e7c62d2f652562b7f502f84e6995af6e03ab3e
PaintRock <6126@holbertonstudents.com>
2023-06-14 09:57:19 -0500

    amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -24,23 +24,6 @@ class TestAmenity(unittest.TestCase):
         '''method to check if instance initializes'''
         self.assertIsNotNone(self.amenity) 
         
-    def test_attributes(self):
-        '''method to test if updates take place in save'''
-        self.assertEqual(self.amenity.name, "")
-        self.assertTrue(hasattr(self.amenity, "created_at"))
-        self.assertTrue(hasattr(self.amenity, "id"))
-        self.amenity.save()
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
-
-    def test___str__(self):
-        '''method to check that dict printing instance'''
-        example = "[{}] ({}) {}".format(self.__class__.__name__,
-                                        self.id, self.__dict__)
-        self.assertEqual(print(self.amenity), print(example))
-
-    def test__repr__(self):
-        '''method to print attributes of dictionary'''
-        self.assertIsNotNone(self.amenity.__str__())
-    
+  
 if __name__ == '__main__':
     unittest.main()
