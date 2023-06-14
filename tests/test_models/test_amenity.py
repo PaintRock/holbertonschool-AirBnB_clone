ecc82a798200182bb2e62b8f046e3e18b5ad5384
PaintRock <6126@holbertonstudents.com>
2023-06-13 22:49:38 -0500

    amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -22,7 +22,25 @@ class TestAmenity(unittest.TestCase):
 
     def test___init__(self):
         '''method to check if instance initializes'''
-        self.assertIsNotNone(self.amenity)   
+        self.assertIsNotNone(self.amenity) 
+        
+     def test_attributes(self):
+        '''method to test if updates take place in save'''
+        self.assertEqual(self.amenity.name, "")
+        self.assertTrue(hasattr(self.amenity, "created_at"))
+        self.assertTrue(hasattr(self.amenity, "id"))
+        self.amenity.save()
+        self.assertTrue(hasattr(self.amenity, "updated_at"))
+
+    def test___str__(self):
+        '''method to check that dict printing instance'''
+        example = "[{}] ({}) {}".format(self.__class__.__name__,
+                                        self.id, self.__dict__)
+        self.assertEqual(print(self.amenity), print(example))
+
+    def test__repr__(self):
+        '''method to print attributes of dictionary'''
+        self.assertIsNotNone(self.amenity.__str__())
     
 def test_amenity(self):
     """Empty test"""
