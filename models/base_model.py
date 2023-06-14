935e66012b33a3b05b3072ff9293f5e2949d0576
PaintRock <6126@holbertonstudents.com>
2023-06-14 15:53:47 -0500

    Base Model json

--- models/base_model.py
+++ models/base_model.py
@@ -43,3 +43,9 @@ class BaseModel():
         """String representation function"""
         im_string = self.__class__.__name__
         return ("[{}] ({}) {}".format(im_string, self.id, self.__dict__))
+
+    def to_json(self):
+        """Return a JSON-serializable representation of the object"""
+        json_dict = self.to_dict()
+        json_dict['__class__'] = self.__class__.__name__
+        return json_dict
