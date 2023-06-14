61ff2aa85e0ed7b96deab827e1357709b62a7937
PaintRock <6126@holbertonstudents.com>
2023-06-14 11:46:25 -0500

    test_place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -25,6 +25,21 @@ class TestPlace(unittest.TestCase):
     def test___init__(self):
         """method to check if instance intializes"""
         self.assertIsNotNone(self.place)
+        
+    def test_attr(self):
+        
+        self.assertEqual(self.place.city_id, "")
+        self.assertEqual(self.place.user_id, "")
+        self.assertEqual(self.place.name, "")
+        self.assertEqual(self.place.description, "")
+        self.assertEqual(self.place.number_rooms, 0)
+        self.assertEqual(self.place.number_bathrooms, 0)
+        self.assertEqual(self.place.max_guest, 0)
+        self.assertEqual(self.place.price_by_night, 0)
+        self.assertEqual(self.place.latitude, 0.0)
+        self.assertEqual(self.place.longitude, 0.0)
+        self.assertEqual(self.place.amenity_ids, "")
+        self.place.save
     
 if __name__ == '__main__':
     unittest.main()
