5caa397d53602c2ce3ea32314134ae4111283a34
PaintRock <6126@holbertonstudents.com>
2023-06-14 15:24:28 -0500

    Userjson

--- models/user.py
+++ models/user.py
@@ -10,3 +10,9 @@ class User(BaseModel):
     last_name = ""
     password = ""
     email = ""
+
+    def to_json(self):
+        """Return a JSON-serializable representation of the object"""
+        json_dict = self.to_dict()
+        json_dict['__class__'] = self.__class__.__name__
+        return json_dict
