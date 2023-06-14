6ff669cf530e27b2f946805845efd44a849267ef
PaintRock <6126@holbertonstudents.com>
2023-06-14 15:37:46 -0500

    hub

--- models/state.py
+++ models/state.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """AirBnB State class from BaseModel"""
 from models.base_model import BaseModel
-
+import json
 
 class State(BaseModel):
     """State class from BaseModel"""
     name = ""
+    def to_json(self):
+        """Return a JSON-serializable representation of the object"""
+        json_dict = self.to_dict()
+        json_dict['__class__'] = self.__class__.__name__
+        return json_dict
