b9d84995a61a505b339a2f9856c14c680b361f1b
PaintRock <6126@holbertonstudents.com>
2023-06-14 17:29:25 -0500

    return True instead of slef.eixt

--- console.py
+++ console.py
@@ -26,7 +26,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        return self.exit()
+        return True
 
     def do_EOF(self, arg):
         """EOF command to exit the program"""
