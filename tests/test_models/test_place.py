1fb2df8392ff58ac7adeac8ab04951f4f5d7e501
PaintRock <6126@holbertonstudents.com>
2023-06-13 21:55:27 -0500

    place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -6,6 +6,25 @@ class TestPlace(unittest.TestCase):
     """TestPlace class documentation"""
 
 
+    def setUp(self):
+        """method to set up instance of Place/json file"""
+        self.place = Place()
+        
+    def tearDown(self):
+        """method to teardown instance of Place/json file"""
+        if os.path.exists("file.json"):
+            try:
+                os.remove("file.json")
+            except:
+                pass
+
+    def test___init__(self):
+        """method to check if instance intializes"""
+        self.assertIsNotNone(self.place)
+
+if __name__ == '__main__':
+    unittest.main()
+
 def test_place():
     """Empty test"""
     pass
