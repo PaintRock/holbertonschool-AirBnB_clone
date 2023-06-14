83d1d91e06dc6749da7a91a69936af045d930e7a
PaintRock <6126@holbertonstudents.com>
2023-06-13 21:08:37 -0500

    dont understand

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,21 +1,53 @@
 """test_city module"""
 import unittest
+import os
+from models.base_model import BaseModel
 from models.city import City
 
+'''test module for City class'''
+
+
 class TestCity(unittest.TestCase):
-    """TestCity class documentation"""
 
+    def setUp(self):
+        '''method to set up instance of city/json file'''
+        self.city = City()
 
-    def test_city(self):
-        """Test to see if city is empty"""
-        cityscape = city()
-        self.assertIsNotEqual(cityscape.id, None)
-        
-    def test_state_id(self):
-        """Test to see if state id is empty"""
-        statescape = state_id()
-        self.assertIsNotEqual(statescape.id, None)
+    def tearDown(self):
+        '''method to tear down instance of city/json file'''
+        if os.path.exists("file.json"):
+            try:
+                os.remove("file.json")
+            except:
+                pass
 
+    def test___init__(self):
+        '''method to check if instance initializes'''
+        self.assertIsNotNone(self.city)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_attributes(self):
+        '''method to test if updates take place in save'''
+        self.assertEqual(self.city.name, "")
+        self.assertTrue(hasattr(self.city, "created_at"))
+        self.assertFalse(hasattr(self.city, "updated_at"))
+        self.assertTrue(hasattr(self.city, "id"))
+        self.city.save()
+        self.assertTrue(hasattr(self.city, "updated_at"))
+
+    def test_to_json(self):
+        '''method to check that the to_json function returns'''
+        example_tojson = City.to_json(self.city)
+        self.assertEqual(type(example_tojson), dict)
+
+    def test___str__(self):
+        '''method to check that dict printing instance'''
+        example = "[{}] ({}) {}".format(self.__class__.__name__,
+                                        self.id, self.__dict__)
+        self.assertEqual(print(self.city), print(example))
+
+    def test__repr__(self):
+        '''method to print attributes of dictionary'''
+        self.assertIsNotNone(self.city.__str__())
+
+if __name__ == "__main__":
+    main())
