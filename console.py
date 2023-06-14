6c9c46e35fee88cea1f413f90cb126b971a778e4
PaintRock <6126@holbertonstudents.com>
2023-06-14 13:45:49 -0500

    imports

--- console.py
+++ console.py
@@ -6,6 +6,15 @@ from models.base_model import BaseModel
 from models.engine import file_storage
 import os
 import sys
+from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
