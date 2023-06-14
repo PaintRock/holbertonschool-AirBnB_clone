b96c3568e040e02d04710653103160e438fc660b
PaintRock <6126@holbertonstudents.com>
2023-06-14 18:07:53 -0500

    ppp

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -42,7 +42,8 @@ class TestReview(unittest.TestCase):
         review.place_id = "789"
         review.text = "Test review"
 
-        expected_str = "[Review] (123) {'id': '123', 'user_id': '456', 'place_id': '789', 'text': 'Test review'}"
+        expected_str = "[Review] (123) {'id': '123', 'user_id': '456'," \
+            "'place_id': '789', 'text': 'Test review'}"
         self.assertEqual(str(review), expected_str)
         
    
